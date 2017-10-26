5c1e34ff96a8eb077dda0d0cffa6b27b25c64140
kmboyle <kevin.michael.boyle@gmail.com>
2017-10-26 15:55:36 -0400

    set up appointment scheduler

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom';
 //import './index.css';
 import App from './App';
 import Cover from './Cover';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render( < App /> , document.getElementById('root'));
+ReactDOM.render( 
+<MuiThemeProvider>
+    < App />
+    </MuiThemeProvider>, document.getElementById('root'));
--- client/src/pages/Appointments/Appointments.js
+++ client/src/pages/Appointments/Appointments.js
@@ -2,17 +2,93 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
+import Dialog from 'material-ui/Dialog'
+import DatePicker from 'material-ui/DatePicker'
+import TimePicker from "material-ui/TimePicker"
+import TextField from 'material-ui/TextField'
+import FlatButton from 'material-ui/FlatButton'
+import RaisedButton from 'material-ui/RaisedButton';
+import moment from 'moment'
+import axios from 'axios'
 
 class Appointments extends Component {
 
 
+state = {
+  appointments: [],
+  confirmationModalOpen: false,
+  validEmail: true,
+  validPhone: true,
+  firstName:"",
+  lastName:"",
+  date: "",
+  slot: "",
+  confirmationModalOpen: false
+}
+
+  handleSubmit() {
+    const appointment = {
+      date: moment(this.state.appointmentDate).format('YYYY-DD-MM'),
+      slot: this.state.appointmentSlot,
+      name: this.state.firstName + ' ' + this.state.lastName,
+      email: this.state.email,
+      phone: this.state.phone
+    }
+    axios.post('api/appointments', appointment)
+    .then(response => this.setState({ confirmationSnackbarMessage: "Appointment succesfully added!", confirmationSnackbarOpen: true, processed: true }))
+    .catch(err => {
+      console.log(err)
+      return this.setState({ confirmationSnackbarMessage: "Appointment failed to save.", confirmationSnackbarOpen: true })
+    })
+  }
+
+  validateEmail(email) {
+    const regex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+    return regex.test(email) ? this.setState({ email: email, validEmail: true }) : this.setState({ validEmail: false })
+  }
+
+  validatePhone(phoneNumber) {
+    const regex = /^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/
+    return regex.test(phoneNumber) ? this.setState({ phone: phoneNumber, validPhone: true }) : this.setState({ validPhone: false })
+  }
+
+  componentWillMount() {
+  
+        axios.get('api/appointments').then(res => {
+          console.log(res.data.data)
+            })
+  }
+
+  renderAppointmentConfirmation() {
+    const spanStyle = { color: '#00bcd4' }
+    return <section>
+      <p>Name: <span style={spanStyle}>{this.state.firstName} {this.state.lastName}</span></p>
+      <p>Number: <span style={spanStyle}>{this.state.phone}</span></p>
+      <p>Email: <span style={spanStyle}>{this.state.email}</span></p>
+      <p>Appointment: <span style={spanStyle}>{moment(this.state.date).format('dddd[,] MMMM Do[,] YYYY')}</span> at <span style={spanStyle}>{moment().hour(9).minute(0).add(this.state.slot, 'hours').format('h:mm a')}</span></p>
+    </section>
+  }
+
+
   render() {
+    const contactFormFilled = this.state.firstName && this.state.lastName && this.state.phone && this.state.email && this.state.validPhone && this.state.validEmail && this.state.date && this.state.slot
+    const modalActions = [
+      <FlatButton
+        label="Cancel"
+        primary={false}
+        onClick={() => this.setState({ confirmationModalOpen : false})} />,
+      <FlatButton
+        label="Confirm"
+        primary={true}
+        onClick={() => this.handleSubmit()} />
+    ]
+    
     return (
       <Container fluid>
         <section>
           <div class="card mb-4">
             <div class="card-block">
-              <h3 class="card-title">Calendar</h3>
+              <h3 class="card-title">Make an Appointment</h3>
               
               <div class="dropdown card-title-btn-container">
                 <button class="btn btn-sm btn-subtle dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><em class="fa fa-cog"></em></button>
@@ -21,8 +97,64 @@ class Appointments extends Component {
                   <a class="dropdown-item" href="#"><em class="fa fa-thumb-tack mr-1"></em> Pin Window</a>
                   <a class="dropdown-item" href="#"><em class="fa fa-remove mr-1"></em> Close Window</a></div>
               </div>
-              
-              <h6 class="card-subtitle mb-2 text-muted">What's coming up</h6>
+              <h4>Select a Day:</h4>
+              <DatePicker
+                      style={{
+                        marginTop: 10,
+                        marginLeft: 10
+                      }}
+                      hintText="Select a date"
+                      onChange={(evt, newValue) => this.setState({ date: newValue })}/>
+                    
+              <h4>Select a Start Time (Between 9am and 5pm):</h4>
+              <TimePicker 
+              minutesStep={60}
+              onChange={(evt, newValue) => this.setState({ slot: newValue })}/>
+              <h4>Share your contact information with us and we'll send you a reminder:</h4>
+              <section>
+                    <TextField
+                      style={{ display: 'block' }}
+                      name="first_name"
+                      hintText="First Name"
+                      floatingLabelText="First Name"
+                      onChange={(evt, newValue) => this.setState({ firstName: newValue })}/>
+                    <TextField
+                      style={{ display: 'block' }}
+                      name="last_name"
+                      hintText="Last Name"
+                      floatingLabelText="Last Name"
+                      onChange={(evt, newValue) => this.setState({ lastName: newValue })}/>
+                    <TextField
+                      style={{ display: 'block' }}
+                      name="email"
+                      hintText="name@mail.com"
+                      floatingLabelText="Email"
+                      errorText={this.state.validEmail ? null : 'Enter a valid email address'}
+                      onChange={(evt, newValue) => this.validateEmail(newValue)}/>
+                    <TextField
+                      style={{ display: 'block' }}
+                      name="phone"
+                      hintText="(888) 888-8888"
+                      floatingLabelText="Phone"
+                      errorText={this.state.validPhone ? null: 'Enter a valid phone number'}
+                      onChange={(evt, newValue) => this.validatePhone(newValue)} />
+                    <RaisedButton
+                      style={{ display: 'block' }}
+                      label={contactFormFilled ? 'Schedule' : 'Fill out your information to schedule'}
+                      labelPosition="before"
+                      primary={true}
+                      fullWidth={true}
+                      onClick={() => this.setState({ confirmationModalOpen: !this.state.confirmationModalOpen })}
+                      disabled={!contactFormFilled}
+                      style={{ marginTop: 20, maxWidth: 100}} />
+                  </section>
+          <Dialog
+            modal={true}
+            open={this.state.confirmationModalOpen}
+            actions={modalActions}
+            title="Confirm your appointment">
+            {this.renderAppointmentConfirmation()}
+          </Dialog>
               
               <div id="calendar"></div>
             </div>
