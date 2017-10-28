87c26e1e31ee8f518ad45f5fa7cf4f72a694fc90
Lisa <lchris19@uncc.edu>
2017-10-28 11:27:00 -0400

    Deleting About Section from Contact

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -10,7 +10,7 @@ class Contact extends Component {
     return (
       <Container fluid>
             <section className="row">
-              <div class="col-lg-12 mb-4">
+              {/* <div class="col-lg-12 mb-4">
                 <div class="card">
                   <div class="card-header"><strong><h3>About</h3></strong></div>
                   
@@ -18,7 +18,7 @@ class Contact extends Component {
                     <p>At Vigor we are revolutionizing the massage therapy and personal training industry by offering a unified solution that no one can rival. The beginning of the journey begins at a question, Where Does It Hurt? In this phase our clients begin by selecting a massage or the patented techniques of movement education we have developed to alleviate their pains. Not a single movement of our client's goes unanalyzed, our professionals are trained to asses patterns in movement in order to accurately solve the problem. With an entirely personalized array of stretches and movements, each client is treated individually with precision and care. As the body recovers our team trains our clients from diagnosing the problem, to solving it, to making sure it isn't privy to resurface. Ultimately to become independent of all therapists, including ourselves, and regain control of their life.</p>
                   </div>
                 </div>
-              </div>
+              </div> */}
     
               <div className="col-sm-12">
                 <section className="row">
