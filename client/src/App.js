9d0fd42596af2f22b5e9b642240d336027c2932e
Muhammad <mchaudh2@uncc.edu>
2017-10-29 13:47:27 -0400

    addign pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dash from "./pages/Dash";
 import Appointments from "./pages/Appointments";
+import Payments from "./pages/Payments";
 import LandingPage from "./pages/LandingPage";
 import Contact from "./pages/Contact";
+import About from "./pages/About";
 import NoMatch from "./pages/NoMatch";
 
 const App = () =>
@@ -13,6 +15,8 @@ const App = () =>
         <Route exact path="/" component={Dash} />
         {<Route exact path="/cover" component={LandingPage} />}
         {<Route exact path="/appointments" component={Appointments} />}
+        {<Route exact path="/payments" component={Payments} />}
+        {<Route exact path="/about" component={About} />}
         {<Route exact path="/contact" component={Contact} />}
         {<Route component={NoMatch} />}
       </Switch>
