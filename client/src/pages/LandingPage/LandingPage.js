04c5c893a11695d270a16791d1ecc41f5eacc1ba
Muhammad <mchaudh2@uncc.edu>
2017-11-05 14:54:50 -0500

    finished cover-page

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -49,22 +49,13 @@ state = {
   };
   render() {
     return (
-<div>        
-  <nav class="navbar navbar-expand-lg navbar-light bg-default">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <a class="navbar-brand cover-brand" href="#">Vigor</a>
-    </div>
-  </nav>
   <Container fluid>
-    <div className="text-center">
+    <div className="cover-div text-center">
       <h3 className="cover-heading">Vigor</h3>
       <p className="cover-text">Helping You Become Independent Of All Therapist, Including Ourselves</p>
+      <a href="login"><button className="btn btn-dark">Enter</button></a>
     </div>
   </Container>
-</div>
     );
   }
 }
